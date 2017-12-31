de228aeae8066210886eaedca2c2a8bbbfb87385
ikostan <igorkostan@gmail.com>
2017-12-31 00:18:08 -0800

    Prototype classes added

---
+++ prototype_sample/prototype.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+class Prototype:
+
+    def __init__(self):
+        pass
+
+    def register_obj(self):
+        pass
+
+    def clone(self):
+        pass
---
+++ prototype_sample/test_prototype.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+def main():
+    """
+        The prototype class has four different methods, and we'll start with the init method.
+        In the init method, all we're doing is simply creating this dictionary object.
+        The dictionary object will be containing the objects that will be cloned.
+        So let's create the dictionary object by typing self._objects,
+        and then the second method is register object.
+    """
+
+    print('Prototype pattern:\n')
+
+
+if __name__ == '__main__':
+    main()
