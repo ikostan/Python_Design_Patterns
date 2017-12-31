57b0a875a2f56cd9dd0d1058ff55a8d1c7efea3d
ikostan <igorkostan@gmail.com>
2017-12-30 18:50:58 -0800

    Car class

---
+++ classes/patterns/builder/car.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+class Car:
+    """Car product"""
+
+    def __init__(self):
+        self._model = None
+        self._tires = None
+        self._engine = None
+
+    def __str__(self):
+        return '{} {} {}'.format(self._model, self._tires, self._engine)
