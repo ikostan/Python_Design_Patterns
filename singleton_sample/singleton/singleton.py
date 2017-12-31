03c6f961e3f5c57777a8a008b82084bc2aef5ba8
ikostan <igorkostan@gmail.com>
2017-12-30 19:31:21 -0800

    Internal structure refactoring

---
+++ singleton_sample/singleton/singleton.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from singleton_sample.singleton import Borg
+
+
+class Singleton(Borg):
+
+    def __init__(self, **kwargs):
+        """Updates an attributes dictionary by inserting a new key-value pairs"""
+        Borg.__init__(self)
+        self._shared_state.update(kwargs)
+
+    def __str__(self):
+        """Returns the attribute dictionary for printing"""
+        return str(self._shared_state)
