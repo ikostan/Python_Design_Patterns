68646b03d8d805cb618db3ad262c7a2db63ce7d6
ikostan <igorkostan@gmail.com>
2017-12-30 18:12:30 -0800

    Refactoring

---
+++ classes/animals/dog.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from classes.animals.pet import Pet
+
+
+class Dog(Pet):
+    """A simple Dog class"""
+    def __init__(self, name):
+        Pet.__init__(self, 'woof', name)
+
+    def __str__(self):
+        return "Dog"
